72545946335ac68ed5400dd6616fca20a237fd63
SionOwen <sionowen34@gmail.com>
2015-02-03 19:18:08 -0700

    created an initial text enter button with limited functionality. Created some basic styling for the quote rating website

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-$("<div class='Quotes container'>")
+var outerBorder= $("<div class='outerBorder'>please enter your quote here!</div>");
+var textContainer= $("<div class='textContainer'></div>");
+var textInput= $("<textarea class='textInput'>");
+var textEnter= $("<input class='textEnter' type='submit'>");
+var quoteCreation= $(".quoteCreation");
+
+
+textEnter.on('click', function(event) {
+var newQuote = textInput.val();
+var addedQuote= "<div class= 'quoteCreation'><p class='textContainer'>"+newQuote+"</p></div>";
+
+$("body").append(addedQuote);
+
+});
+
+
 
 
 
 
 $(document).on('ready', function() {
-  
 });
+
+
+
+
+
+
+
+textContainer.append(textInput);
+quoteCreation.append(outerBorder);
+quoteCreation.append(textContainer);
+quoteCreation.append(textEnter);
