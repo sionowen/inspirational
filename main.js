a0cb3eb22f8503b79eaab6ec25ab77a776fa83c1
SionOwen <sionowen34@gmail.com>
2015-02-04 17:10:02 -0700

    this is the last version of the project that is not coded in an object oriented style, going to start refactoring code

--- main.js
+++ main.js
@@ -3,14 +3,25 @@ var textContainer= $("<div class='textContainer'></div>");
 var textInput= $("<textarea class='textInput'>");
 var textEnter= $("<input class='textEnter' type='submit'>");
 var quoteCreation= $(".quoteCreation");
+var authorLog= $("<input class='authorLog' type='text'>Author</input>");
+var authorArray = [];
+
 
 
 textEnter.on('click', function(event) {
-var newQuote = textInput.val();
-var addedQuote= "<div class= 'quoteCreation'><p class='textContainer'>"+newQuote+"</p></div>";
+	var newQuote = textInput.val();
+	var newAuthor = authorLog.val();
+	var quoteObject = {
+		author: authorLog.val(),
+		text: textInput.val()
+	};
+	authorArray.push(quoteObject);
+	console.log(authorArray);
+	var addedQuote= "<div class= 'quoteCreation'><p class='textContainer'>"  + newQuote + "<br>" + '-' + newAuthor + "</p></div>";
 
 $("body").append(addedQuote);
-
+$(".textInput").val("");
+$(".authorLog").val("");
 });
 
 
@@ -31,3 +42,4 @@ textContainer.append(textInput);
 quoteCreation.append(outerBorder);
 quoteCreation.append(textContainer);
 quoteCreation.append(textEnter);
+quoteCreation.append(authorLog);
